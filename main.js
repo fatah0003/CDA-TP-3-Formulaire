53bc4f3b423f9861fea2d9d4e399c018e5e98e91
fatah0003 <fatahutd@gmail.com>
2025-04-17 12:09:36 +0200

    version1.0

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ const participants = document.querySelector('#participants');
 const button = document.querySelector('button');
 const nameError = document.querySelector('#name-error')
 const timeError = document.querySelector('#time-error')
-
+const participantsError = document.querySelector('#participants-error')
+const dateError = document.querySelector('#date-error')
+const reservations = []
 const stringRegex = /^[a-zA-Z]+$/
 
 form.addEventListener('submit', function (e) {
@@ -19,8 +21,29 @@ form.addEventListener('submit', function (e) {
     if(time.value.slice(0, 2) < 9 || time.value.slice(0, 2) > 18){
         timeError.style.display = "block"
     }
+    if(participants.value < 1 || participants.value > 10){
+        participantsError.style.display = "block"
+    }
+    reservations.forEach(element => {
+        if(element.date === date.value && element.time === time.value){
+        dateError.style.display = "block"
+            
+        }
+        
+    });
     
-    
+    if(form){
+        reservations.push(
+                {
+                  name: fName.value,
+                  date: date.value,
+                  time: time.value,
+                  participants: parseInt(participants.value, 10)
+                }
+        )
+        console.log(reservations);
+        
+    }
   });
 
 
