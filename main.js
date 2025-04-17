f9e7dee45e0a854cfc282f9c43eeeb2207122ac7
fatah0003 <fatahutd@gmail.com>
2025-04-17 14:15:56 +0200

    version2.0

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const form = document.querySelector('#booking-form');
 const fName = document.querySelector('#name');
-const date = document.querySelector('#date');
+const dateReservation = document.querySelector('#date');
+const dateReservationError = document.querySelector('#date-reservation-error');
 const time = document.querySelector('#time');
 const participants = document.querySelector('#participants');
 const button = document.querySelector('button');
@@ -15,35 +16,58 @@ const stringRegex = /^[a-zA-Z]+$/
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
+
+    // Cacher les messages d'erreurs
+    nameError.style.display = "none";
+    timeError.style.display = "none";
+    participantsError.style.display = "none";
+    dateError.style.display = "none";
+    dateReservationError.style.display = "none"
+
+    let isValid = true;
     if(fName.value.trim().length < 3 || !stringRegex.test(fName.value.trim())){
         nameError.style.display = "block"
+        isValid = false; // pour empecher d'enregestrer le reservation
     }
     if(time.value.slice(0, 2) < 9 || time.value.slice(0, 2) > 18){
         timeError.style.display = "block"
+    isValid = false;
     }
     if(participants.value < 1 || participants.value > 10){
         participantsError.style.display = "block"
+        isValid = false;
     }
     reservations.forEach(element => {
-        if(element.date === date.value && element.time === time.value){
+        if(element.dateReservation === dateReservation.value && element.time === time.value){
         dateError.style.display = "block"
+        isValid = false;
             
         }
         
-    });
+    })
+    const date = new Date(dateReservation.value);
+    if(date <= Date.now()){
+        dateReservationError.style.display = "block"
+        isValid = false;
+        
+    }
     
-    if(form){
-        reservations.push(
+        if(isValid){
+            reservations.push(
                 {
                   name: fName.value,
-                  date: date.value,
+                  dateReservation: dateReservation.value,
                   time: time.value,
                   participants: parseInt(participants.value, 10)
                 }
         )
-        console.log(reservations);
+        console.log(reservations)
         
-    }
+        
+        }else {
+            console.log("Formulaire invalide, réservation non ajoutée");
+            
+        }
   });
 
 
